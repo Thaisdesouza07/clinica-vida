75d4f0909b30ae0fd28319ab1851b5bb72b3ac86
Thais Souza <thais.souza.tech@gmail.com>
2026-02-12 00:56:52 -0300

    Remove arquivos ex07x

--- ex074.py
+++
@@ -1,9 +0,0 @@
-from random import randint
-numeros = (randint(1,10), randint(1,10), randint(1,10),
-    randint(1,10), randint(1,10))
-
-print(f'Os valores sorteados: ', end='')
-for n in numeros:
-    print(f' {n} ', end='')
-print(f'\nO maior valor sorteador foi {max(numeros)}')
-print(f'O menor valor sorteador foi {min(numeros)}')
